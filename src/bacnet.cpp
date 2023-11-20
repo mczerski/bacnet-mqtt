75486ab74b322e98f59ab449847721f43ee27fad
Marek Czerski <m.czerski@ap-tech.pl>
2023-11-20 19:35:02 +0100

    scov + read object list

--- src/bacnet.cpp
+++ src/bacnet.cpp
@@ -1,6 +1,9 @@
 #include <cstdint>
 #include <iostream>
-#include "bacnet.h"
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include "bacnet.hpp"
 
 /* buffer used for receive */
 static uint8_t Rx_Buf[MAX_MPDU] = { 0 };
@@ -14,102 +17,42 @@ static struct mstimer datalink_timer = { 0 };
 
 #define BAC_ADDRESS_MULT 1
 
-//TODO: replace with c++ way
-struct address_entry {
-    struct address_entry *next;
-    uint8_t Flags;
-    uint32_t device_id;
-    unsigned max_apdu;
-    BACNET_ADDRESS address;
+struct device_entry {
+    std::string name;
+    std::vector<BACNET_OBJECT_ID> object_list;
 };
+std::unordered_map<uint32_t, device_entry> device_map;
 
-static struct address_table {
-    struct address_entry *first;
-    struct address_entry *last;
-} Address_Table = { 0 };
-
-static struct address_entry *alloc_address_entry(void)
+static void device_map_add(uint32_t device_id)
 {
-    struct address_entry *rval;
-    rval = (struct address_entry *)calloc(1, sizeof(struct address_entry));
-    if (Address_Table.first == 0) {
-        Address_Table.first = Address_Table.last = rval;
-    } else {
-        Address_Table.last->next = rval;
-        Address_Table.last = rval;
+    if (device_map.count(device_id))
+        return;
+
+    if (BACnet_Debug_Enabled) {
+        fprintf(stderr, "Reading object list from device %u\n", device_id);
     }
-    return rval;
-}
-
-static void address_table_add(
-    uint32_t device_id, unsigned max_apdu, BACNET_ADDRESS *src)
-{
-    struct address_entry *pMatch;
-    uint8_t flags = 0;
-
-    pMatch = Address_Table.first;
-    while (pMatch) {
-        if (pMatch->device_id == device_id) {
-            if (bacnet_address_same(&pMatch->address, src)) {
-                return;
-            }
-            flags |= BAC_ADDRESS_MULT;
-            pMatch->Flags |= BAC_ADDRESS_MULT;
-        }
-        pMatch = pMatch->next;
+    auto invoke_id = Send_Read_Property_Request(
+        device_id,
+        OBJECT_DEVICE,
+        device_id,
+        PROP_OBJECT_LIST,
+        BACNET_ARRAY_ALL
+    );
+    if (invoke_id == 0) {
+        fprintf(stderr, "Failed to read object list from device %u\n", device_id);
+        return;
     }
-
-    pMatch = alloc_address_entry();
-
-    pMatch->Flags = flags;
-    pMatch->device_id = device_id;
-    pMatch->max_apdu = max_apdu;
-    pMatch->address = *src;
-
     return;
 }
 
-static void my_i_am_handler(
-    uint8_t *service_request, uint16_t service_len, BACNET_ADDRESS *src)
+static void i_am_handler(uint8_t *service_request, uint16_t service_len, BACNET_ADDRESS *src)
 {
-    int len = 0;
-    uint32_t device_id = 0;
-    unsigned max_apdu = 0;
-    int segmentation = 0;
-    uint16_t vendor_id = 0;
-    unsigned i = 0;
-
-    (void)service_len;
-    len = iam_decode_service_request(
-        service_request, &device_id, &max_apdu, &segmentation, &vendor_id);
-    if (BACnet_Debug_Enabled) {
-        fprintf(stderr, "Received I-Am Request");
+    handler_i_am_add(service_request, service_len, src);
+    uint32_t device_id;
+    bool found = address_get_device_id(src, &device_id);
+    if (found) {
+        device_map_add(device_id);
     }
-    if (len != -1) {
-        if (BACnet_Debug_Enabled) {
-            fprintf(stderr, " from %lu, MAC = ", (unsigned long)device_id);
-            if ((src->mac_len == 6) && (src->len == 0)) {
-                fprintf(stderr, "%u.%u.%u.%u %02X%02X\n", (unsigned)src->mac[0],
-                    (unsigned)src->mac[1], (unsigned)src->mac[2],
-                    (unsigned)src->mac[3], (unsigned)src->mac[4],
-                    (unsigned)src->mac[5]);
-            } else {
-                for (i = 0; i < src->mac_len; i++) {
-                    fprintf(stderr, "%02X", (unsigned)src->mac[i]);
-                    if (i < (src->mac_len - 1)) {
-                        fprintf(stderr, ":");
-                    }
-                }
-                fprintf(stderr, "\n");
-            }
-        }
-        address_table_add(device_id, max_apdu, src);
-    } else {
-        if (BACnet_Debug_Enabled) {
-            fprintf(stderr, ", but unable to decode it.\n");
-        }
-    }
-
     return;
 }
 
@@ -134,6 +77,118 @@ static void MyRejectHandler(
         bactext_reject_reason_name(reject_reason));
 }
 
+static void handle_object_list(uint32_t device_id, const BACNET_READ_PROPERTY_DATA& data) {
+    if (BACnet_Debug_Enabled) {
+        fprintf(stderr, "Received object list from device %d\n", device_id);
+    }
+    std::vector<BACNET_OBJECT_ID> object_list;
+    uint8_t *application_data = data.application_data;
+    int application_data_len = data.application_data_len;
+    for (;;) {
+        BACNET_APPLICATION_DATA_VALUE value;
+        int len = bacapp_decode_known_property(
+            application_data,
+            application_data_len,
+            &value,
+            data.object_type,
+            data.object_property
+        );
+
+        if (len < 0) {
+            fprintf(stderr, "RP Ack: unable to decode! %s:%s\n",
+                bactext_object_type_name(data.object_type),
+                bactext_property_name(data.object_property)
+            );
+            break;
+        }
+        object_list.push_back(value.type.Object_Id);
+
+        if (len < application_data_len) {
+            application_data += len;
+            application_data_len -= len;
+        } else {
+            device_map[device_id] = {.object_list = std::move(object_list)};
+            if (BACnet_Debug_Enabled) {
+                fprintf(stderr, "Adding new device entry for device %d\n", device_id);
+            }
+            if (BACnet_Debug_Enabled) {
+                fprintf(stderr, "Reading device name from device %u\n", device_id);
+            }
+            int invoke_id = Send_Read_Property_Request(
+                device_id,
+                OBJECT_DEVICE,
+                device_id,
+                PROP_OBJECT_NAME,
+                BACNET_ARRAY_ALL
+            );
+            if (invoke_id == 0) {
+                fprintf(stderr, "Failed to read device name from device %u\n", device_id);
+                return;
+            }
+            break;
+        }
+    }
+}
+
+static void handle_device_name(uint32_t device_id, const BACNET_READ_PROPERTY_DATA& data) {
+    if (BACnet_Debug_Enabled) {
+        fprintf(stderr, "Received device name from device %d\n", device_id);
+    }
+    BACNET_APPLICATION_DATA_VALUE value;
+    int len = bacapp_decode_known_property(
+        data.application_data,
+        data.application_data_len,
+        &value,
+        data.object_type,
+        data.object_property
+    );
+
+    if (len < 0) {
+        fprintf(stderr, "RP Ack: unable to decode! %s:%s\n",
+            bactext_object_type_name(data.object_type),
+            bactext_property_name(data.object_property)
+        );
+        return;
+    }
+    device_map[device_id].name = characterstring_value(&value.type.Character_String);
+}
+/** Handler for a ReadProperty ACK.
+ * @ingroup DSRP
+ * Doesn't actually do anything, except, for debugging, to
+ * print out the ACK data of a matching request.
+ *
+ * @param service_request [in] The contents of the service request.
+ * @param service_len [in] The length of the service_request.
+ * @param src [in] BACNET_ADDRESS of the source of the message
+ * @param service_data [in] The BACNET_CONFIRMED_SERVICE_DATA information
+ *                          decoded from the APDU header of this message.
+ */
+static void read_property_ack_handler(
+    uint8_t *service_request,
+    uint16_t service_len,
+    BACNET_ADDRESS *src,
+    BACNET_CONFIRMED_SERVICE_ACK_DATA *service_data
+)
+{
+    BACNET_READ_PROPERTY_DATA data;
+    int len = rp_ack_decode_service_request(service_request, service_len, &data);
+    if (len < 0) {
+        fprintf(stderr, "Read property response decode failed!\n");
+        return;
+    }
+
+    uint32_t device_id;
+    bool found = address_get_device_id(src, &device_id);
+    if (not found)
+        return;
+
+    if (data.object_type == OBJECT_DEVICE and data.object_property == PROP_OBJECT_LIST)
+        handle_object_list(device_id, data);
+
+    if (data.object_type == OBJECT_DEVICE and data.object_property == PROP_OBJECT_NAME)
+        handle_device_name(device_id, data);
+}
+
 static void init_service_handlers(void)
 {
     Device_Init(NULL);
@@ -143,76 +198,14 @@ static void init_service_handlers(void)
        It is required to send the proper reject message... */
     apdu_set_unrecognized_service_handler_handler(handler_unrecognized_service);
     /* we must implement read property - it's required! */
-    apdu_set_confirmed_handler(
-        SERVICE_CONFIRMED_READ_PROPERTY, handler_read_property);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_READ_PROPERTY, handler_read_property);
     /* handle the reply (request) coming back */
-    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_I_AM, my_i_am_handler);
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_I_AM, i_am_handler);
+    /* handle the data coming back from confirmed requests */
+    apdu_set_confirmed_ack_handler(SERVICE_CONFIRMED_READ_PROPERTY, read_property_ack_handler);
     /* handle any errors coming back */
     apdu_set_abort_handler(MyAbortHandler);
     apdu_set_reject_handler(MyRejectHandler);
-}
-
-//TODO: replace with c++ way
-static void print_macaddr(uint8_t *addr, int len)
-{
-    int j = 0;
-
-    while (j < len) {
-        if (j != 0) {
-            printf(":");
-        }
-        printf("%02X", addr[j]);
-        j++;
-    }
-    while (j < MAX_MAC_LEN) {
-        printf("   ");
-        j++;
-    }
-}
-
-//TODO: replace with c++ way
-static void print_address_cache(void)
-{
-    BACNET_ADDRESS address;
-    unsigned total_addresses = 0;
-    unsigned dup_addresses = 0;
-    struct address_entry *addr;
-    uint8_t local_sadr = 0;
-
-    /*  NOTE: this string format is parsed by src/address.c,
-       so these must be compatible. */
-
-    printf(";%-7s  %-20s %-5s %-20s %-4s\n", "Device", "MAC (hex)", "SNET",
-        "SADR (hex)", "APDU");
-    printf(";-------- -------------------- ----- -------------------- ----\n");
-
-    addr = Address_Table.first;
-    while (addr) {
-        bacnet_address_copy(&address, &addr->address);
-        total_addresses++;
-        if (addr->Flags & BAC_ADDRESS_MULT) {
-            dup_addresses++;
-            printf(";");
-        } else {
-            printf(" ");
-        }
-        printf(" %-7u ", addr->device_id);
-        print_macaddr(address.mac, address.mac_len);
-        printf(" %-5hu ", address.net);
-        if (address.net) {
-            print_macaddr(address.adr, address.len);
-        } else {
-            print_macaddr(&local_sadr, 1);
-        }
-        printf(" %-4u ", (unsigned)addr->max_apdu);
-        printf("\n");
-
-        addr = addr->next;
-    }
-    printf(";\n; Total Devices: %u\n", total_addresses);
-    if (dup_addresses) {
-        printf("; * Duplicate Devices: %u\n", dup_addresses);
-    }
 }
 
 void bacnet_init() {
@@ -233,6 +226,9 @@ void bacnet_init() {
         .net = BACNET_BROADCAST_NETWORK,
         .len = 0
     };
+    if (BACnet_Debug_Enabled) {
+        fprintf(stderr, "Sending Who-Is Request");
+    }
     Send_WhoIs_To_Network(&dest, -1, -1);
 }
 
@@ -257,8 +253,10 @@ void bacnet_task() {
         mstimer_reset(&datalink_timer);
     }
     if (mstimer_expired(&apdu_timer)) {
+        if (BACnet_Debug_Enabled) {
+            fprintf(stderr, "Sending Who-Is Request");
+        }
         Send_WhoIs_To_Network(&dest, -1, -1);
         mstimer_reset(&apdu_timer);
-        print_address_cache();
     }
 }
